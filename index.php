573f498f1bb6c671edf1170a9dd931ac101a8ab9
Mitlond <comp122012016@mail.ru>
2016-03-09 15:20:42 +0300

    ver1
    ver1

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+<?php
+	$a=9;
+	$b=3;
+	$result=$a+$b;
+	echo "summa= ". $result;
+ ?>
+	
+</body>
+</html>
